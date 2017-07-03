60b1c9ed4e571994ee602d20dcb2dbf76028f6e7
Rahbee Alvee <rahbee.alvee@gmail.com>
2017-07-03 22:41:16 +1000

    Sass loader added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
+import './style.scss';
 function component() {
   var element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
