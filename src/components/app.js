95524ba861b832885828f9a1ec47abafdf473528
Rahbee Alvee <rahbee.alvee@gmail.com>
2018-01-17 22:27:08 +1100

    eslint, jest integration

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import SearchBar from '../containers/search_bar';
 import WeatherList from '../containers/weather_list';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <SearchBar />
-        <WeatherList />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <SearchBar />
+    <WeatherList />
+  </div >
+);
+
+export default App;
